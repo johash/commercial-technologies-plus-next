9a28830e6f3794ba2830c971af9e6ef6c9d16656
Johash Perera <johash@commercialtp.com>
2022-05-27 01:53:44 +0530

    fix rel

--- components/LocationMap/LocationMap.js
+++ components/LocationMap/LocationMap.js
@@ -9,7 +9,7 @@ const LocationMap = () => {
           "https://www.google.com/maps/place/Commercial+Technologies+Plus+(Private)+Limited/@6.675524,79.9220358,17z/data=!3m1!4b1!4m5!3m4!1s0x3ae2481e3ca64fcf:0xa1279f3b88acc7d5!8m2!3d6.6755272!4d79.9242198"
         }
         target="_blank"
-        // className={classes.ImageContainer}
+        rel="noreferrer"
       >
         <div className={classes.ImageContainer}>
           <Image
