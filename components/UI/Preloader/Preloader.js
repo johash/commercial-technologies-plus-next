164bca5236b420c410dcdaee18e703282b806a2f
Johash Perera <johash@commercialtp.com>
2022-06-13 22:30:17 +0530

    add preloader

---
+++ components/UI/Preloader/Preloader.js
@@ -0,0 +1,26 @@
+import classes from "./Preloader.module.scss";
+import Lottie from "react-lottie";
+import PreloaderAnimation from "../../../public/assets/animation/preloader.json";
+
+const Preloader = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: PreloaderAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <section className={classes.Preloader}>
+      <div className={classes.Container}>
+        <div className={classes.LottiContainer}>
+          <Lottie options={defaultOptions} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Preloader;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
 import "../styles/globals.css";
+import React from "react";
+import Preloader from "../components/UI/PreLoader/Preloader";
 
 import { NextUIProvider } from "@nextui-org/react";
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <>
+      {!loading ? (
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      ) : (
+        <Preloader />
+      )}
+    </>
   );
 }
 
