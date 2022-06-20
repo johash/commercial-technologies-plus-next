f408e28e7b34a7d4ba938aab6c7d422382990955
Johash Perera <johash@commercialtp.com>
2022-06-20 07:58:07 +0530

    add terms of service page

---
+++ components/TermsOfServiceHeader/TermsOfServiceHeader.js
@@ -0,0 +1,13 @@
+import classes from "./TermsOfServiceHeader.module.scss";
+
+const TermsOfServiceHeader = () => {
+  return (
+    <div className={classes.TermsOfServiceHeader}>
+      <div className={classes.Container}>
+        <h1>Terms of Service</h1>
+      </div>
+    </div>
+  );
+};
+
+export default TermsOfServiceHeader;
---
+++ pages/terms-of-service/index.js
@@ -0,0 +1,14 @@
+import Layout from "../../components/Layout/Layout";
+import TermsOfServiceHeader from "../../components/TermsOfServiceHeader/TermsOfServiceHeader";
+import TermsOfServiceBody from "../../components/TermsOfServiceBody/TermsOfServiceBody";
+
+const TermsOfServicePage = () => {
+  return (
+    <Layout>
+      <TermsOfServiceHeader />
+      <TermsOfServiceBody />
+    </Layout>
+  );
+};
+
+export default TermsOfServicePage;
