fa2ccd042a956a9b8b61c38ff1820b0478a2c6e0
Johash Perera <johash@commercialtp.com>
2022-06-28 10:43:07 +0530

    add swiper in awards page

--- components/AwardsCarousel/AwardsCarousel.js
+++ components/AwardsCarousel/AwardsCarousel.js
@@ -1,53 +1,94 @@
 import classes from "./AwardsCarousel.module.scss";
 import Image from "next/image";
 
+import SwiperCore, { Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 const AwardsCarousel = () => {
+  SwiperCore.use([Autoplay]);
+
   return (
     <section className={classes.AwardsCarousel}>
-      <div className={classes.Container}>
-        <div className={classes.ImageContainer}>
-          <Image
-            src="/assets/awards/c1.png"
-            layout="fill"
-            className={classes.Image}
-          />
-        </div>
-        <div className={classes.ImageContainer}>
-          <Image
-            src="/assets/awards/c2.png"
-            layout="fill"
-            className={classes.Image}
-          />
-        </div>
-        <div className={classes.ImageContainer}>
-          <Image
-            src="/assets/awards/c3.png"
-            layout="fill"
-            className={classes.Image}
-          />
-        </div>
-        <div className={classes.ImageContainer}>
-          <Image
-            src="/assets/awards/c4.png"
-            layout="fill"
-            className={classes.Image}
-          />
-        </div>
-        <div className={classes.ImageContainer}>
-          <Image
-            src="/assets/awards/c5.png"
-            layout="fill"
-            className={classes.Image}
-          />
-        </div>
-        <div className={classes.ImageContainer}>
-          <Image
-            src="/assets/awards/c6.png"
-            layout="fill"
-            className={classes.Image}
-          />
-        </div>
-      </div>
+      <Swiper
+        className={classes.AwardsSwiperContainer}
+        autoplay={{
+          delay: 1500,
+        }}
+        direction="horizontal"
+        slidesPerView="auto"
+        spaceBetween={5}
+        loop
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        <SwiperSlide className={classes.AwardsSwiperSlide}>
+          <div className={classes.ImageContainer}>
+            <Image
+              src="/assets/awards/c1.png"
+              layout="fill"
+              className={classes.Image}
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={classes.ImageContainer}>
+            <Image
+              src="/assets/awards/c2.png"
+              layout="fill"
+              className={classes.Image}
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={classes.ImageContainer}>
+            <Image
+              src="/assets/awards/c3.png"
+              layout="fill"
+              className={classes.Image}
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={classes.ImageContainer}>
+            <Image
+              src="/assets/awards/c4.png"
+              layout="fill"
+              className={classes.Image}
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={classes.ImageContainer}>
+            <Image
+              src="/assets/awards/c5.png"
+              layout="fill"
+              className={classes.Image}
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={classes.ImageContainer}>
+            <Image
+              src="/assets/awards/c6.png"
+              layout="fill"
+              className={classes.Image}
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };
